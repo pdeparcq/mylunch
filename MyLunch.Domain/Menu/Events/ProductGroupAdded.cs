badf9c519f32e83d391f4f49b2a7901683921074
Pieter Deparcq <pdeparcq@gmail.com>
2019-11-17 08:45:56 +0100

    Can add product groups

---
+++ MyLunch.Domain/Menu/Events/ProductGroupAdded.cs
@@ -0,0 +1,11 @@
+﻿using Kledex.Domain;
+using System.Collections.Generic;
+
+namespace MyLunch.Domain.Menu.Events
+{
+    public class ProductGroupAdded : DomainEvent
+    {
+        public string Name { get; set; }
+        public List<MenuOption> Options { get; set; }
+    }
+}
--- MyLunch.Domain/Menu/ProductGroup.cs
+++ MyLunch.Domain/Menu/ProductGroup.cs
@@ -1,14 +1,22 @@
-﻿using Kledex.Domain;
+﻿using Guards;
+using Kledex.Domain;
 using System;
 using System.Collections.Generic;
 
 namespace MyLunch.Domain.Menu
 {
-    public class ProductGroup : AggregateRoot
+    public class ProductGroup : Entity
     {
-        public Guid RestaurantId { get; private set; }
-        public string Title { get; private set; }
-        public int MenuOrder { get; private set; }
+        public string Name { get; private set; }
         public List<MenuOption> Options { get; private set; }
+
+        public ProductGroup(string name, List<MenuOption> options)
+        {
+            Guard.ArgumentNotNullOrEmpty(() => name);
+            Guard.ArgumentNotNull(() => options);
+
+            Name = name;
+            Options = options;
+        }
     }
 }
