55f5b7f067dc6eaaa016f45a6c1be334cb47d123
Pieter Deparcq <pdeparcq@gmail.com>
2019-11-20 21:46:01 +0100

    Almost there

---
+++ MyLunch.Application/Menu/Mappings/RestaurantProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using MyLunch.Application.Menu.ViewModels;
+
+namespace MyLunch.Application.Menu.Mappings
+{
+    public class RestaurantProfile : Profile
+    {
+        public RestaurantProfile()
+        {
+            CreateMap<Entities.Restaurant, RestaurantViewModel>();
+        }
+    }
+}
---
+++ MyLunch.Test/Application/Menu/Services/RestaurantServiceTests.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using Kledex.Events;
+using Kledex.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MyLunch.Application.Menu.EventHandlers;
+using MyLunch.Application.Menu.Mappings;
+using MyLunch.Application.Menu.Services;
+using MyLunch.Domain.Menu.Events;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyLunch.Test.Application.Menu.Services
+{
+    [TestFixture]
+    public class RestaurantServiceTests
+    {
+        [Test]
+        public async Task CanGetRestaurantById()
+        {
+
+            //Register services
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddDbContext<MyLunch.Application.Menu.Entities.MenuDbContext>(options => options.UseInMemoryDatabase(databaseName: "Test"));
+            serviceCollection.AddKledex(typeof(RestaurantRegisteredEventHandler));
+            serviceCollection.AddAutoMapper(typeof(RestaurantProfile).Assembly);
+            serviceCollection.AddLogging(cfg => cfg.AddConsole());
+            serviceCollection.AddTransient<RestaurantService>();
+
+            //Build provider
+            var provider = serviceCollection.BuildServiceProvider();
+
+            //Validate mappers
+            var mapper = provider.GetService<IMapper>();
+            mapper.ConfigurationProvider.AssertConfigurationIsValid();
+
+            //Publish events
+            var publisher = provider.GetService<IEventPublisher>();
+            var restaurantId = Guid.NewGuid();
+            await publisher.PublishAsync(new RestaurantRegistered
+            {
+                AggregateRootId = restaurantId,
+                Name = "Taste it Gent",
+                ContactEmail = new MyLunch.Domain.Shared.EmailAddress("info@taste-it-gent.be")
+            });
+
+            var service = provider.GetService<RestaurantService>();
+
+            var restaurant = await service.GetRestaurantById(restaurantId);
+
+            Assert.IsNotNull(restaurant);
+
+            restaurant.PrettyPrint(Console.Out);
+        }
+    }
+}
